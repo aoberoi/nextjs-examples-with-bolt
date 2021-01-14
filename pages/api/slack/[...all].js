2c98337dc737e6656ca0682c9d7dc9f731c8e563
Ankur Oberoi <aoberoi@gmail.com>
2021-01-14 02:04:41 -0800

    turn on logging

--- pages/api/slack/[...all].js
+++ pages/api/slack/[...all].js
@@ -1,10 +1,11 @@
-import { HTTPReceiver } from '@slack/bolt';
+import { HTTPReceiver, LogLevel } from '@slack/bolt';
 import bolt from '../../../lib/bolt';
 
 const receiver = new HTTPReceiver({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   processBeforeResponse: true,
   endpoints: '/api/slack/events',
+  logLevel: LogLevel.DEBUG,
 });
 
 bolt(receiver);
